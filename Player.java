7782d6b3e0745d3bbded25d4cbf134bcaeeb7998
Your Name <you@example.com>
2025-04-28 18:54:45 -0400

    rooms class added

--- Player.java
+++ Player.java
@@ -1,4 +1,38 @@
 public class Player {
+
+     //Attributes 
+     String name; 
+     double money; 
+     int chances; 
+     String emoji; 
+ 
+ 
+     //Constructor
+ 
+     public void player {
+         this.name = name; 
+         this.money = money; 
+         this.age = age; 
+     
+ 
+     } 
+ 
+     public void assignGuest(Guest guest, Room room) {
+         
+     }
+ 
+ 
+ 
+     spendMoney(double amount)
+     earnMoney(double amount)
+     grabObject(String object)
+     giveObject(String object, Guest guest)
+     goToRoom(int roomNumber)
+     goToLobby()
+     build(String buildingName)
+     loseChance()
+     checkGameOver()
+ 
     
     public static void main(String[] args) {
     }
